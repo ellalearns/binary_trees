cca3c39ba34ec1021f3b75acf67ee45b9694a11f
ellalearns <ellalearnscopy@gmail.com>
2024-04-09 14:40:07 +0100

    task 0 - create new node

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,24 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - creates a new node
+ * @parent: the parent node of the new node
+ * @value: value of new node
+ * Return: the new node
+*/
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+    binary_tree_t *newNode;
+
+    if (!newNode)
+    {
+        return (NULL);
+    }
+
+    newNode->parent = parent;
+    newNode->n = value;
+    newNode->left = NULL;
+    newNode->right = NULL;
+
+    return (newNode);
+}

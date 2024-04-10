8d3fb1016f4895f4d737151bda346f74aa07b309
ellalearns <ellalearnscopy@gmail.com>
2024-04-10 13:18:51 +0100

    task 12 changes

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -20,7 +20,7 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
     }
     else
     {
-        binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right);
+        leaves = binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right);
     }
 
     return (leaves);

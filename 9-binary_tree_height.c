6b5854dee2881ab30a7d7b0446aaa14085dd89dd
ellalearns <ellalearnscopy@gmail.com>
2024-04-10 12:21:56 +0100

    task 9 - get height of binary tree

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,34 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - count the height of a binary tree
+ * @tree: the tree to use
+ * Return: the height of the tree, 0 if null
+*/
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+    size_t left_height = 0;
+    size_t right_height = 0;
+
+    if (tree == NULL)
+    {
+        return (0);
+    }
+
+    if (tree->left != NULL)
+    {
+        left_height = 1 + binary_tree_height(tree->left);
+    }
+
+    if (tree->right != NULL)
+    {
+        right_height = 1 + binary_tree_height(tree->right);
+    }
+
+    if (left_height > right_height)
+    {
+        return (left_height);
+    }
+
+    return (right_height);
+}

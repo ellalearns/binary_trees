9d9c48b4b4a8bc5b0b465f40de04cfe9883a1fb5
ellalearns <ellalearnscopy@gmail.com>
2024-04-10 10:39:39 +0100

    task 3 - delete entire binary tree

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,29 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_delete - deletes an entire binary tree
+ * @tree: the binary tree to be deleted
+*/
+void binary_tree_delete(binary_tree_t *tree)
+{
+    if (tree != NULL)
+    {
+        if (tree->left != NULL)
+    {
+        binary_tree_delete(tree->left);
+    }
+    else
+    {
+        if (tree->right != NULL)
+        {
+            binary_tree_delete(tree->left);
+        }
+        else
+        {
+            free(tree->right);
+        }
+        free(tree->left);
+    }
+    }
+    
+}

9bf93260b42819aa9897f5b5a35021c03d79d95d
ellalearns <ellalearnscopy@gmail.com>
2024-04-10 13:14:45 +0100

    task 12 - count leaf nodes

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,25 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_leaves - counts the leaves in a binary tree
+ * @tree: the binary tree
+ * Return: number  of leaves
+*/
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+    size_t leaves = 0;
+
+    if (tree == NULL)
+    {
+        return (0);
+    }
+
+    binary_tree_leaves(tree->left);
+    if (tree->left == NULL && tree->right == NULL)
+    {
+        leaves++;
+    }
+    binary_tree_leaves(tree->right);
+
+    return (leaves);
+}

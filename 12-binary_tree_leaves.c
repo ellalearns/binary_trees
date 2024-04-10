bfd906adc54c8a2fa9057ffdaae8a5bd3b818003
ellalearns <ellalearnscopy@gmail.com>
2024-04-10 13:21:53 +0100

    betty styles

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -7,21 +7,21 @@
 */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-    size_t leaves = 0;
+size_t leaves = 0;
 
-    if (tree == NULL)
-    {
-        return (0);
-    }
+if (tree == NULL)
+{
+return (0);
+}
 
-    if (tree->left == NULL && tree->right == NULL)
-    {
-        leaves++;
-    }
-    else
-    {
-        leaves = binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right);
-    }
+if (tree->left == NULL && tree->right == NULL)
+{
+leaves++;
+}
+else
+{
+leaves = binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right);
+}
 
-    return (leaves);
+return (leaves);
 }

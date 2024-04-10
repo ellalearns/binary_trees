72938825f3356b15582d0d8907ee7f6209665386
ellalearns <ellalearnscopy@gmail.com>
2024-04-10 12:56:18 +0100

    task 11 - calculate binary tree size

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_size - function to measure size of binary tree
+ * @tree: the tree to measure the size (count of all nodes)
+ * Return: number of nodes (size) or 0 if tree is null
+*/
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+    size_t size = 0;
+
+    if (tree == NULL)
+    {
+        return (0);
+    }
+
+    binary_tree_size(tree->left);
+    size++;
+    binary_tree_size(tree->right);
+
+    return (size);
+}

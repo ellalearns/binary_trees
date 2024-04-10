fb94d910decc11191c20d08b9a73d23162455f86
ellalearns <ellalearnscopy@gmail.com>
2024-04-10 13:01:34 +0100

    task 11 moddds

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -16,6 +16,7 @@ size_t binary_tree_size(const binary_tree_t *tree)
 
     size = 1 + binary_tree_size(tree->left);
     size = 1 + binary_tree_size(tree->right);
+    size++;
 
     return (size);
 }

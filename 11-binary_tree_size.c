7c3f5843af745e5805f1f1f7fe72475e8dcd244a
ellalearns <ellalearnscopy@gmail.com>
2024-04-10 13:03:35 +0100

    task 11 changesss

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -14,9 +14,7 @@ size_t binary_tree_size(const binary_tree_t *tree)
         return (0);
     }
 
-    size = 1 + binary_tree_size(tree->left);
-    size = 1 + binary_tree_size(tree->right);
-    size++;
+    size = binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1;
 
     return (size);
 }

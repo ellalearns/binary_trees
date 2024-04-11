dc3a0f158125d7c06759809bd61bb3e120ecf623
ellalearns <ellalearnscopy@gmail.com>
2024-04-11 11:20:44 +0100

    task 15 - check if binary tree is full && task 14 changes

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -7,8 +7,8 @@
 */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-    size_t left_height = 0;
-    size_t right_height = 0;
+    int left_height = 0;
+    int right_height = 0;
     int balance = 0;
 
     if (tree == NULL)
@@ -16,10 +16,44 @@ int binary_tree_balance(const binary_tree_t *tree)
         return (0);
     }
 
-    left_height = binary_tree_height(tree->left);
-    right_height = binary_tree_height(tree->right);
+    left_height = (int) binary_tree_height(tree->left);
+    right_height = (int) binary_tree_height(tree->right);
 
     balance = left_height - right_height;
 
     return (balance);
+}
+
+
+/**
+ * binary_tree_height - count the height of a binary tree
+ * @tree: the tree to use
+ * Return: the height of the tree, 0 if null
+*/
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+size_t left_height = 0;
+size_t right_height = 0;
+
+if (tree == NULL)
+{
+return (0);
+}
+
+if (tree->left != NULL)
+{
+left_height = 1 + binary_tree_height(tree->left);
+}
+
+if (tree->right != NULL)
+{
+right_height = 1 + binary_tree_height(tree->right);
+}
+
+if (left_height > right_height)
+{
+return (left_height);
+}
+
+return (right_height);
 }
---
+++ 15-binary_tree_is_full.c
@@ -0,0 +1,39 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_full - checks if a binary tree is full
+ * @tree: the tree (each node should have 0 or 2 children)
+ * Return: 1 if true, 0 if false or tree is null
+*/
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+    int is_full = 1;
+    int is_full_left = 1;
+    int is_full_right = 1;
+
+    if (tree == NULL)
+    {
+        return (0);
+    }
+
+    if (tree->left != NULL && tree->right == NULL)
+    {
+        is_full = 0;
+    }
+
+    if (tree->left == NULL && tree->right != NULL)
+    {
+        is_full = 0;
+    }
+
+    is_full_left = binary_tree_is_full(tree->left);
+    is_full_right = binary_tree_is_full(tree->right);
+
+    if (is_full_left == 1 && is_full_right == 1)
+    {
+        if (is_full == 1)
+        {
+            return (1);
+        }
+    }
+}
